c6dfbb8e6d9a1f2338cacbde8cd1be39be5ac09a
soctrungkien <112756562+soctrungkien@users.noreply.github.com>
2025-04-08 20:37:57 +0700

    Update Menu.lua

--- Menu.lua
+++ Menu.lua
@@ -46,12 +46,7 @@ loadstring(game:HttpGet("https://raw.githubusercontent.com/soctrungkien/scriptro
 -------TAG
 loadstring(game:HttpGet("https://raw.githubusercontent.com/soctrungkien/scriptroblox/refs/heads/main/Script/tag.lua"))()
 -------MOUSE
--- Get the local player and their mouse
-local player = game.Players.LocalPlayer
-local mouse = player:GetMouse()
-
--- Set the mouse cursor to a custom image (replace with your own asset ID)
-mouse.Icon = "rbxassetid://118074316147407" -- Replace with a valid Roblox asset ID
+loadstring(game:HttpGet("https://raw.githubusercontent.com/soctrungkien/scriptroblox/refs/heads/main/Script/mouse.lua"))()
 ------- BUTTON
     
     AddButton(Tab1o, {
