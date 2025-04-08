2c2f8f67b4a332d44c8b1b82e3d08a896abdc5c1
soctrungkien <112756562+soctrungkien@users.noreply.github.com>
2025-04-08 18:57:48 +0700

    Update antiafk.lua

--- Script/antiafk.lua
+++ Script/antiafk.lua
@@ -4,23 +4,30 @@ local Player = Players.LocalPlayer
 local Character = Player.Character or Player.CharacterAdded:Wait()
 local HumanoidRootPart = Character:WaitForChild("HumanoidRootPart")
 
--- Lưu vị trí ban đầu
-local originalPosition = HumanoidRootPart.Position
-
--- Hàm tạo số ngẫu nhiên từ 0.02 đến 0.07
-local function getRandomOffset()
-    return math.random(2, 7) / 100 -- Chuyển từ 0.02 đến 0.07 studs
-end
-
 -- Vòng lặp vô tận
 while true do
+
+    -- Hàm tạo số ngẫu nhiên từ 0.02 đến 0.07
+        local function getRandomOffset()
+        return math.random(2, 7) / 100 -- Chuyển từ 0.02 đến 0.07 studs
+    end
+
+    -- Hàm tạo số ngẫu nhiên từ 0.001 đến 0.005
+    local function randomtime()
+        return math.random(1, 5) / 1000 -- Chuyển từ 0.001 đến 0.005 studs
+    end
+    
+    -- Lưu vị trí ban đầu
+    local originalPosition = HumanoidRootPart.Position
+    
     -- Di chuyển nhân vật
     local offset = getRandomOffset()
     local newPosition = originalPosition + Vector3.new(offset, 0, 0) -- Di chuyển theo trục X
     HumanoidRootPart.CFrame = CFrame.new(newPosition)
     print("Đã di chuyển " .. offset .. " studs")
 
-    wait(0.001)
+    local wrt = randomtime()
+    wait(wrt)
     
         -- Quay về vị trí ban đầu
     HumanoidRootPart.CFrame = CFrame.new(originalPosition)
@@ -28,6 +35,5 @@ while true do
 
     -- Đợi 10 phút (600 giây)
     wait(600)
-
 
 end
