5341d033f26364c2db39756663c8e948abebe5ee
soctrungkien <112756562+soctrungkien@users.noreply.github.com>
2025-04-09 14:04:32 +0700

    Update mouse.lua

--- Script/mouse.lua
+++ Script/mouse.lua
@@ -3,4 +3,4 @@ local player = game.Players.LocalPlayer
 local mouse = player:GetMouse()
 
 -- Set the mouse cursor to a custom image (replace with your own asset ID)
-mouse.Icon = "rbxassetid://89928340581127" -- Replace with a valid Roblox asset ID
+mouse.Icon = "rbxassetid://118074316147407" -- Replace with a valid Roblox asset ID
