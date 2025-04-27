fe75c52b3f9f35c9123d91b9f2870b888c72e254
soctrungkien <112756562+soctrungkien@users.noreply.github.com>
2025-04-27 10:24:59 +0700

    Update kickap.lua
    Signed-off-by: soctrungkien <112756562+soctrungkien@users.noreply.github.com>

--- Script/kickap.lua
+++ Script/kickap.lua
@@ -1,9 +1,1 @@
-local targetUserIds = {
-    [5219564843] = true
-}
-
-local Notify = "Bạn đã bị ban vĩnh viễn "
-
-if targetUserIds[game:GetService("Players").LocalPlayer.UserId] then
-    game:GetService("Players").LocalPlayer:Kick(Notify)
-end
+game:GetService("Players").LocalPlayer:Kick("Bạn đã bị ban vĩnh viễn")
