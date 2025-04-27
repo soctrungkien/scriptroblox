88a37bed0b2dbd2f57622625764ebe2b3418516b
soctrungkien <112756562+soctrungkien@users.noreply.github.com>
2025-04-27 22:05:56 +0700

    Update Menu.lua
    Signed-off-by: soctrungkien <112756562+soctrungkien@users.noreply.github.com>

--- Menu.lua
+++ Menu.lua
@@ -28,7 +28,7 @@ loadstring(game:HttpGet("https://raw.githubusercontent.com/soctrungkien/scriptro
 
 
 ------MinButtonToggle
-local MinButtonToggle = loadstring(game:HttpGet("https://raw.githubusercontent.com/soctrungkien/scriptroblox/refs/heads/main/Script/MinButtonToggle.lua"))()
+local MinButtonToggle = loadstring(game:HttpGet("https://raw.githubusercontent.com/soctrungkien/scriptroblox/refs/heads/main/Script/MinButtonToggle.lua"))(Fluent, Window)
 local button = MinButtonToggle.Create()
 if button then
     button.Image = "rbxassetid://81249639738070"
