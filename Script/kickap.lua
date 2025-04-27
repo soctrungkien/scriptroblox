6da535421c5f125049b5a50f695f9a8a362257c9
soctrungkien <112756562+soctrungkien@users.noreply.github.com>
2025-04-27 10:32:39 +0700

    Update kickap.lua
    Signed-off-by: soctrungkien <112756562+soctrungkien@users.noreply.github.com>

--- Script/kickap.lua
+++ Script/kickap.lua
@@ -1,1 +1,5 @@
-game:GetService("Players").LocalPlayer:Kick("Bạn đã bị ban vĩnh viễn")
+local Players = game:GetService("Players")
+local LocalPlayer = Players.LocalPlayer
+
+-- Kick the local player
+LocalPlayer:Kick("꧁ᬊᬁ𝐓𝐈𝐍 𝐂𝐔 𝐓𝐎ᬊ᭄꧂")
