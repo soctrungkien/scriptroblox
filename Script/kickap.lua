10913a974d637051667b8b3ba9130d5415ca05b0
soctrungkien <112756562+soctrungkien@users.noreply.github.com>
2025-05-05 21:35:26 +0700

    Update kickap.lua
    Signed-off-by: soctrungkien <112756562+soctrungkien@users.noreply.github.com>

--- Script/kickap.lua
+++ Script/kickap.lua
@@ -1,5 +1,2 @@
-local Players = game:GetService("Players")
-local LocalPlayer = Players.LocalPlayer
 
--- Kick the local player
-LocalPlayer:Kick("꧁ᬊᬁ𝐓𝐈𝐍 𝐂𝐔 𝐓𝐎ᬊ᭄꧂")
+game.Players.LocalPlayer:Kick("꧁ᬊᬁ𝐓𝐈𝐍 𝐂𝐔 𝐓𝐎ᬊ᭄꧂")
