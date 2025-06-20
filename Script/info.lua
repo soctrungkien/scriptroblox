8dea0c36fcb18f94b1cae9aad7f2518ce6b60722
TuiㅤKoㅤCóㅤTên <112756562+soctrungkien@users.noreply.github.com>
2025-06-20 16:56:09 +0700

    Update info.lua
    Signed-off-by:  TuiㅤKoㅤCóㅤTên <112756562+soctrungkien@users.noreply.github.com>

--- Script/info.lua
+++ Script/info.lua
@@ -4,6 +4,7 @@ local placeId = game.PlaceId
 local gameId = game.GameId
 local userId = player.UserId
 local jobId = game.JobId
+local ver = game.PlaceVersion
 
 -- Tạo thông tin
 local info = ""
@@ -11,7 +12,7 @@ info = info .. "ㅤ\n"
 info = info .. "ㅤ\n"
 info = info .. "ㅤ────────────────────────────\n"
 info = info .. "ㅤ\n"
-info = info .. "ㅤ📌 THÔNG TIN GAME HIỆN TẠI\n"
+info = info .. "ㅤ📌 INFO\n"
 info = info .. "ㅤ\n"
 info = info .. "ㅤ────────────────────────────\n"
 info = info .. "ㅤ\n"
@@ -22,6 +23,8 @@ info = info .. "ㅤ\n"
 info = info .. "ㅤ👤 UserId    : " .. userId .. "\n"
 info = info .. "ㅤ\n"
 info = info .. "ㅤ️💻 JobId     : " .. jobId .. "\n"
+info = info .. "ㅤ\n"
+info = info .. "ㅤ🕹️ Version   : " .. ver .. "\n"
 info = info .. "ㅤ\n"
 info = info .. "ㅤ────────────────────────────"
 info = info .. "ㅤ\n"
