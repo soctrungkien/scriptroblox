cab51ca7bffe204547a7f084f9536ce5d04c10b0
TuiㅤKoㅤCóㅤTên <112756562+soctrungkien@users.noreply.github.com>
2025-09-06 10:59:06 +0700

    Update Loader.lua
    Signed-off-by:  TuiㅤKoㅤCóㅤTên <112756562+soctrungkien@users.noreply.github.com>

--- wavehub/Loader.lua
+++ wavehub/Loader.lua
@@ -1,12 +1,12 @@
 repeat wait() until game:IsLoaded()
 local PlaceId = game.PlaceId
-if PlaceId == 2753915549 or PlaceId == 4442272183 or PlaceId == 7449423635 then
+if PlaceId == 2753915549 or PlaceId == 4442272183 or PlaceId == 7449423635 then --Blox Fruits
     --chưa làm
-elseif PlaceId == 126884695634066 then
+elseif PlaceId == 126884695634066 then --[🧚‍♀️] Trồng một khu vườn 🌶️
     --cũng chưa luôn
 else
-elseif PlaceId == 85896571713843 then
+elseif PlaceId == 85896571713843 then --☯️Bong bóng Gum Simulator INFINITY
     loadstring(game:HttpGet("https://raw.githubusercontent.com/soctrungkien/scriptroblox/refs/heads/main/wavehub/BubbleGumSimulator.lua"))()
-else
+else --Universal
     loadstring(game:HttpGet("https://raw.githubusercontent.com/soctrungkien/scriptroblox/refs/heads/main/wavehub/Universal.lua"))()
 end
