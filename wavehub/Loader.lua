eb86e5eb2b4e674729b403f0ff7e54697707b0a5
TuiㅤKoㅤCóㅤTên <112756562+soctrungkien@users.noreply.github.com>
2025-09-27 22:58:10 +0700

    Update Loader.lua
    Signed-off-by:  TuiㅤKoㅤCóㅤTên <112756562+soctrungkien@users.noreply.github.com>

--- wavehub/Loader.lua
+++ wavehub/Loader.lua
@@ -1,4 +1,5 @@
 repeat wait() until game:IsLoaded()
+
 print([[
  ___       __   ________  ___      ___ _______   ___  ___  ___  ___  ________     
 |\  \     |\  \|\   __  \|\  \    /  /|\  ___ \ |\  \|\  \|\  \|\  \|\   __  \    
@@ -8,19 +9,28 @@ print([[
    \ \____________\ \__\ \__\ \__/ /     \ \_______\ \__\ \__\ \_______\ \_______\
     \|____________|\|__|\|__|\|__|/       \|_______|\|__|\|__|\|_______|\|_______|
 ]])
+
 local _ENV = (getgenv or getrenv or getfenv)()
 local BETA_VERSION = BETA_VERSION or _ENV.BETA_VERSION
 local PlaceId = game.PlaceId
-if PlaceId == 2753915549 or PlaceId == 4442272183 or PlaceId == 7449423635 then --Blox Fruits
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/soctrungkien/scriptroblox/refs/heads/main/wavehub/Universal.lua"))() --chưa làm
-elseif PlaceId == 126884695634066 then --Trồng một khu vườn
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/soctrungkien/scriptroblox/refs/heads/main/wavehub/Universal.lua"))() --cũng chưa luôn
-elseif PlaceId == 70743305607680 then --BuildBlox
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/soctrungkien/scriptroblox/refs/heads/main/wavehub/BuildBlox.lua"))()
-elseif PlaceId == 127742093697776 then --Plants Vs Brainrots
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/soctrungkien/scriptroblox/refs/heads/main/wavehub/PvB.lua"))()
-elseif PlaceId == 85896571713843 then --Bong bóng Gum Simulator INFINITY
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/soctrungkien/scriptroblox/refs/heads/main/wavehub/BubbleGumSimulator.lua"))()
-else --Universal
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/soctrungkien/scriptroblox/refs/heads/main/wavehub/Universal.lua"))()
+
+local scripts = {
+    [2753915549] = "Universal.lua", -- Blox Fruits
+    [4442272183] = "Universal.lua", --sea 2 
+    [7449423635] = "Universal.lua", --sea 3 
+    [126884695634066] = "Universal.lua", -- Trồng một khu vườn
+    [70743305607680] = "BuildBlox.lua",  -- BuildBlox
+    [127742093697776] = "PvB.lua",       -- Plants Vs Brainrots
+    [85896571713843] = "BubbleGumSimulator.lua" -- BubbleGumSimulator
+}
+
+local scriptName = scripts[PlaceId] or "Universal.lua"
+local url = "https://raw.githubusercontent.com/soctrungkien/scriptroblox/refs/heads/main/wavehub/" .. scriptName 
+
+local success, result = pcall(function()
+    return loadstring(game:HttpGet(url))()
+end)
+
+if not success then
+    warn("[wavehub/Loader.lua] Failed to load script:", result)
 end
