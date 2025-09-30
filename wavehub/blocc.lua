880e5a1d66fba8492ebb7c03c2ebc6fa5f9ec8bf
TuiㅤKoㅤCóㅤTên <112756562+soctrungkien@users.noreply.github.com>
2025-09-30 22:10:03 +0700

    Create blocc.lua
    Signed-off-by:  TuiㅤKoㅤCóㅤTên <112756562+soctrungkien@users.noreply.github.com>

---
+++ wavehub/blocc.lua
@@ -0,0 +1,27 @@
+local camera = workspace.CurrentCamera
+local screenSize = camera.ViewportSize
+local desiredWidth = 200
+local desiredHeight = 130
+local maxWidth = math.min(desiredWidth, screenSize.X * 0.9)
+local maxHeight = math.min(desiredHeight, screenSize.Y * 0.9)
+local ReGui = loadstring(game:HttpGet('https://raw.githubusercontent.com/depthso/Dear-ReGui/refs/heads/main/ReGui.lua'))()
+local Window = ReGui:Window({
+	Title = "Wave Hub",
+	Size = UDim2.fromOffset(maxWidth, maxHeight)
+})
+local bf = false
+Window:Checkbox({
+	Value = false,
+	Label = "Free Blocc",
+	Callback = function(self, Value: boolean)
+		bd = Value
+	end
+})
+task.spawn(function()
+while true do
+if bf then
+workspace:WaitForChild("FreePad"):WaitForChild("F2PSpawnRequest"):FireServer()
+end
+task.wait(0.06)
+end
+end)
