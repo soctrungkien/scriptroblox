55c5109adbe8bd9820112b7102d70f22a078d05c
TuiㅤKoㅤCóㅤTên <112756562+soctrungkien@users.noreply.github.com>
2026-02-22 20:05:56 +0700

    Update Loader.lua to adjust script URL and handling
    Signed-off-by:  TuiㅤKoㅤCóㅤTên <112756562+soctrungkien@users.noreply.github.com>

--- wavehub/Loader.lua
+++ wavehub/Loader.lua
@@ -14,27 +14,28 @@ print([[
 
 local _ENV = (getgenv or getrenv)()
 local PlaceId = game.PlaceId
-local loadername = "Wave Hub Loader"
 
 local function Loadstring(httpurl) 
 _ENV.loadername = loadername
-loadstring(game:HttpGet(httpurl, true))(loadername, PlaceId) 
+loadstring(game:HttpGet(httpurl, true))(loadername, PlaceId, scriptName) 
 end
 
+if scriptName == nil then
 local scripts = {
-    [2753915549] = "Universal", -- Blox Fruits
-    [4442272183] = "Universal", --sea 2 
-    [7449423635] = "Universal", --sea 3 
-    [126884695634066] = "Universal", -- Trồng một khu vườn
-    [70743305607680] = "BuildBlox",  -- BuildBlox
-    [127742093697776] = "PvB",       -- Plants Vs Brainrots
-    [85896571713843] = "BubbleGumSimulator", -- BubbleGumSimulator
-    [87067157937644] = "blocc", --blocc (idk)
-    [124398083342642] = "rideacartdownaslide", --rideacartdownaslide
+--  [2753915549] = "wavehub/Universal", -- Blox Fruits
+--  [4442272183] = "wavehub/Universal", --sea 2 
+--  [7449423635] = "wavehub/Universal", --sea 3 
+--  [126884695634066] = "wavehub/Universal", -- Trồng một khu vườn
+    [70743305607680] = "wavehub/BuildBlox",  -- BuildBlox
+    [127742093697776] = "wavehub/PvB",       -- Plants Vs Brainrots
+    [85896571713843] = "wavehub/BubbleGumSimulator", -- BubbleGumSimulator
+    [87067157937644] = "wavehub/blocc", --blocc (idk)
+    [124398083342642] = "wavehub/rideacartdownaslide", --rideacartdownaslide
 }
+end
 
 local scriptName = scripts[PlaceId] or "Universal"
-local url = "https://raw.githubusercontent.com/soctrungkien/scriptroblox/refs/heads/main/wavehub/" .. scriptName .. ".lua"
+local url = "https://raw.githubusercontent.com/soctrungkien/scriptroblox/refs/heads/main/" .. scriptName .. ".lua"
 
 local success, result = pcall(function()
     return Loadstring(url)
