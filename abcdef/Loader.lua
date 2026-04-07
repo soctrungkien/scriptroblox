41894e07a8e7d0c37980d44f18461317c51e1b71
TuiㅤKoㅤCóㅤTên <112756562+soctrungkien@users.noreply.github.com>
2026-04-07 12:05:06 +0700

    Update and rename Loader.lua to Loader.lua
    Signed-off-by:  TuiㅤKoㅤCóㅤTên <112756562+soctrungkien@users.noreply.github.com>

--- abcdef/Loader.lua
+++ abcdef/Loader.lua
@@ -12,22 +12,22 @@ loadstring(game:HttpGet(httpurl, true))(loadername, PlaceId, scriptName)
 end
 
 scripts = {
-    [70743305607680] = "wavehub/BuildBlox",  -- BuildBlox
-    [127742093697776] = "wavehub/PvB",       -- Plants Vs Brainrots
-    [85896571713843] = "wavehub/BubbleGumSimulator", -- BubbleGumSimulator
-    [87067157937644] = "wavehub/blocc", --blocc (idk)
-    [124398083342642] = "wavehub/rideacartdownaslide", --rideacartdownaslide
-    [108180976346281] = "wavehub/crosstheroad", --crosstheroad
-    [112555408629538] = "wavehub/lowcortisolobby", --lowcortisolobby
+    [70743305607680] = "abcdef/BuildBlox",  -- BuildBlox
+    [127742093697776] = "abcdef/PvB",       -- Plants Vs Brainrots
+    [85896571713843] = "abcdef/BubbleGumSimulator", -- BubbleGumSimulator
+    [87067157937644] = "abcdef/blocc", --blocc (idk)
+    [124398083342642] = "abcdef/rideacartdownaslide", --rideacartdownaslide
+    [108180976346281] = "abcdef/crosstheroad", --crosstheroad
+    [112555408629538] = "abcdef/lowcortisolobby", --lowcortisolobby
 }
 
-if scriptName == "wavehub/Loader" then
+if scriptName == "abcdef/Loader" then
     warn("nah bro is loop")
     _ENV.scriptName = nil
     return
 end
 
-local scriptName = scriptName or scripts[PlaceId] or "wavehub/Universal"
+local scriptName = scriptName or scripts[PlaceId] or "abcdef/Universal"
 local url = "https://raw.githubusercontent.com/soctrungkien/scriptroblox/refs/heads/main/" .. scriptName .. ".lua"
 
 --print("Loader " .. scriptName)
